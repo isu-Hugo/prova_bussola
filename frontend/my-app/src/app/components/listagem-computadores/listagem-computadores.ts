5617984c2bce450684410a87cb1076be771cf2c1
Hugo <ehugo209@gmail.com>
2026-04-08 22:08:36 -0300

    ajustar listagem

--- frontend/my-app/src/app/components/listagem-computadores/listagem-computadores.ts
+++ frontend/my-app/src/app/components/listagem-computadores/listagem-computadores.ts
@@ -1,21 +1,24 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Listagem } from '../../services/listagem';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-listagem-computadores',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './listagem-computadores.html',
   styleUrl: './listagem-computadores.css',
 })
 export class ListagemComputadores {
   private service = inject(Listagem);
 
-  computadores = [{}];
+
+
+  computadores = signal([]);
 
   listarComputadores(){
     this.service.getComputadores().subscribe({
       next: (res) =>{
-        this.computadores = res;
+        this.computadores.set(res);
         console.log(res)
       },
       error: (er) =>{
